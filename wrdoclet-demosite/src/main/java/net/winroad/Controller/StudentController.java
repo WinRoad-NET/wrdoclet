73650449b633bf566a822d5ebe5aa3bddb8c4121
AdamsLee <adamslee@outlook.com>
2015-02-16 14:45:54 +0800

    add demo site

---
+++ wrdoclet-demosite/src/main/java/net/winroad/Controller/StudentController.java
@@ -0,0 +1,29 @@
+package net.winroad.Controller;
+
+import net.winroad.Models.Student;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+	@RequestMapping(value = "{name}", method = RequestMethod.GET)
+	public @ResponseBody
+	Student getStudentInJSON(@PathVariable String name) {
+		Student s = new Student();
+		s.setName(name);
+		s.setAge(11);
+		s.setWeight(100);
+		return s;
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public @ResponseBody
+	Object addStudent(Student person) {
+		return person;
+	}
+}
---
+++ wrdoclet-demosite/src/main/java/net/winroad/Models/Student.java
@@ -0,0 +1,31 @@
+package net.winroad.Models;
+
+public class Student {
+	private String name;
+	private int age;
+	private int weight;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+}
