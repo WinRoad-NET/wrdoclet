9d7976a87be153990eed4da98723db9590a01d5d
AdamsLee <adamslee@outlook.com>
2017-05-16 22:26:11 +0800

    move wrdocletbase out from wrdoclet

--- wrdocletbase/src/main/java/net/winroad/wrdoclet/WRWriterFactory.java
+++
@@ -1,7 +0,0 @@
-package net.winroad.wrdoclet;
-
-import com.sun.tools.doclets.internal.toolkit.WriterFactory;
-
-public interface WRWriterFactory extends WriterFactory {
-	public FreemarkerWriter getFreemarkerWriter();
-}
--- wrdocletbase/src/main/java/net/winroad/wrdoclet/data/ParameterType.java
+++
@@ -1,6 +0,0 @@
-package net.winroad.wrdoclet.data;
-
-public enum ParameterType {
-	Request,
-	Response
-}
